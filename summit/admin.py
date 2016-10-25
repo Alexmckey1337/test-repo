c42813653a1f432fe3cfba241431a7a46a455424
pluton <amirrpp@yandex.ru>
2016-10-25 14:36:54 +0300

    admin for summit anket note

--- summit/admin.py
+++ summit/admin.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
-from .models import SummitAnket, Summit, SummitType
+from .models import SummitAnket, Summit, SummitType, SummitAnketNote
 from .resources import SummitAnketResource
 
 
@@ -13,11 +13,17 @@ class SummitTypeAdmin(admin.ModelAdmin):
     filter_horizontal = ('consultants',)
 
 
+class SummitAnketNoteInline(admin.TabularInline):
+    model = SummitAnketNote
+
+
 class SummitAnketAdmin(ImportExportModelAdmin):
     list_display = ('name', 'user', 'summit', 'code',)
     list_filter = ('summit', 'user__department', 'protected',)
     search_fields = ['code', 'user__last_name', ]
     resource_class = SummitAnketResource
+
+    inlines = [SummitAnketNoteInline, ]
 
     class Meta:
         model = SummitAnket
