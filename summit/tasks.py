8ba6b31a35e70db467e5af604854356dd677ad5d
pluton <amirrpp@yandex.ru>
2016-11-18 17:44:52 +0200

    [hotfix] fix barcode

--- summit/tasks.py
+++ summit/tasks.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8
 from __future__ import unicode_literals
 
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+
 from edem.celery import app
+from summit.utils import generate_ticket
 from .resources import make_table
 
 
@@ -9,3 +14,27 @@ from .resources import make_table
 def generate():
     make_table()
     return True
+
+
+@app.task(ignore_relust=True, max_retries=10, default_retry_delay=10)
+def send_ticket(summit_anket):
+    template_name = 'email/summit_ticket.html'
+    ctx = {}
+    main_email = settings.EMAIL_HOST_USER or ''
+    recipient_list = [summit_anket.user.email]
+
+    html_template = get_template(template_name)
+    subject = ctx.get('subject', 'Билет')
+    message = ctx.get('message', 'Билен на саммит {}'.format(summit_anket.summit))
+    from_email = 'Билет <{email}>'.format(email=main_email)
+    html_message = html_template.render(ctx)
+
+    mail = EmailMultiAlternatives(subject, message, from_email, recipient_list)
+    mail.attach_alternative(html_message, 'text/html')
+
+    mail.attach('{} ({}).pdf'.format(
+        summit_anket.user.fullname, summit_anket.code),
+        generate_ticket(summit_anket.code),
+        'application/pdf')
+
+    mail.send()
