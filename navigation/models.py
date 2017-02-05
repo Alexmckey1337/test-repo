f52c99f1e1c4de3f049ebf3508fa917b2f1f7b39
pluton <amirrpp@yandex.ru>
2017-02-05 13:52:59 +0200

    [refactoring] move navigation receivers funcs to navigation/receivers.py

--- navigation/models.py
+++ navigation/models.py
@@ -2,8 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.db.models import signals
-from django.dispatch import receiver
 from django.utils.encoding import python_2_unicode_compatible
 
 
@@ -76,25 +74,3 @@ class Column(models.Model):
     class Meta:
         ordering = ['number']
         unique_together = ('table', 'columnType')
-
-
-@receiver(signals.post_save, sender=ColumnType)
-def sync_column(sender, instance, **kwargs):
-    tables = Table.objects.all()
-    user_columns = list()
-    for table in tables:
-        user_columns.append(Column(
-            table=table, columnType=instance, number=instance.number, active=instance.active))
-
-    Column.objects.bulk_create(user_columns)
-
-
-@receiver(signals.post_save, sender=Table)
-def sync_table(sender, instance, **kwargs):
-    exist_columns = set(instance.columns.values_list('columnType', flat=True))
-    user_new_columns = list()
-    for column_type in ColumnType.objects.exclude(pk__in=exist_columns):
-        user_new_columns.append(Column(
-            table=instance, columnType=column_type, number=column_type.number, active=column_type.active))
-
-    Column.objects.bulk_create(user_new_columns)
