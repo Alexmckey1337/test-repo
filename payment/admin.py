725e2bd50f0d639596f58d14d35b0363406c6bd8
pluton <amirrpp@yandex.ru>
2017-03-24 13:50:26 +0200

    update admin panel for payments

--- payment/admin.py
+++ payment/admin.py
@@ -8,6 +8,8 @@ from payment.models import Payment, Currency
 
 class PaymentAdmin(admin.ModelAdmin):
     readonly_fields = ('effective_sum',)
+    list_display = ('__str__', 'manager', 'sum', 'effective_sum', 'sent_date', 'created_at')
+    search_fields = ('manager__last_name',)
 
 
 admin.site.register(Payment, PaymentAdmin)
