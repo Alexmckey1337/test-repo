21c79a5eb07dfb9d33c223286464adce6623d55b
pluton <amirrpp@yandex.ru>
2017-04-06 17:57:28 +0300

    added api for delete summit profile

--- payment/serializers.py
+++ payment/serializers.py
@@ -68,6 +68,23 @@ class PaymentShowSerializer(serializers.ModelSerializer):
                   'manager', 'purpose')
 
 
+class PaymentShowWithUrlSerializer(serializers.HyperlinkedModelSerializer):
+    currency_sum = CurrencySerializer()
+    currency_rate = CurrencySerializer()
+    manager = ManagerSerializer()
+    created_at = serializers.DateTimeField(format='%d.%m.%Y %H:%M')
+    sent_date = serializers.DateTimeField(format='%d.%m.%Y')
+    purpose = PurposeRelatedField(read_only=True)
+
+    class Meta:
+        model = Payment
+        fields = ('id', 'url', 'sum', 'effective_sum',
+                  'sum_str', 'effective_sum_str', 'operation',
+                  'currency_sum', 'currency_rate', 'rate', 'description',
+                  'created_at', 'sent_date',
+                  'manager', 'purpose')
+
+
 class PaymentDealShowSerializer(PaymentShowSerializer):
     purpose = DealRelatedField(read_only=True, source='object_id')
     purpose_fio = serializers.CharField()
