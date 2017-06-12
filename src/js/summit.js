5db796b77d5967889b4da175f704dfa7a437ec6d
Ruslan Sapizhuk <ruslan.sapizhuk@gmail.com>
2017-06-12 09:53:50 +0300

    clear

--- src/js/summit.js
+++ src/js/summit.js
@@ -104,7 +104,8 @@
         });
     });
     $('#download').on('click', function () {
-        let stat = new PrintMasterStat($('#summitsTypes').find('.active').data('id'));
+        let summitId = $('#summitsTypes').find('.active').data('id');
+        let stat = new PrintMasterStat(summitId);
         stat.show();
     });
 
