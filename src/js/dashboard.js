75caa7724471998907ac6f1d179888d06d4b5d40
Viacheslav <mukha.viacheslav@gmail.com>
2017-06-14 21:24:31 +0300

    Fix

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -65,22 +65,21 @@
                 chosenClass: "sortable-chosen",
                 dragClass: "sortable-drag",
                 group: "localStorage-example",
-                store: {
-                    get: function (sortable) {
-                        let order = localStorage.getItem(sortable.options.group.name);
-                        return order ? order.split('|') : [];
-                    },
-                    set: function (sortable) {
-                        // let order = sortable.toArray();
-                        // localStorage.setItem(sortable.options.group.name, order.join('|'));
-                    }
-                },
+                // store: {
+                //     get: function (sortable) {
+                //         let order = localStorage.getItem(sortable.options.group.name);
+                //         return order ? order.split('|') : [];
+                //     },
+                //     set: function (sortable) {
+                //         // let order = sortable.toArray();
+                //         // localStorage.setItem(sortable.options.group.name, order.join('|'));
+                //     }
+                // },
                 filter: ".vision",
 	            onFilter: function (evt) {
 		            let item = evt.item,
 			            ctrl = evt.target;
 		            if (Sortable.utils.is(ctrl, ".vision")) {
-		                console.log(item);
                         $(item).find('.vision').toggleClass('active');
                         $(item).closest('.well').toggleClass('hide');
 		            }
@@ -110,18 +109,29 @@
                 $('.dashboard').find('.edit-desk').removeClass('active');
                 $('.dashboard').find('.drop').removeClass('active');
                 let state = sortable.option("disabled");
-                let order = sortable.toArray();
+                // let order = sortable.toArray();
                 sortable.option("disabled", !state);
-                localStorage.setItem(sortable.options.group.name, order.join('|'));
+                // localStorage.setItem(sortable.options.group.name, order.join('|'));
 
-                let $arrWell = $('.dashboard').find('.well'),
-                    arrHideWell = [];
-                $arrWell.each(function (index) {
-                    if ($(this).hasClass('hide')) {
-                        arrHideWell.push(index);
-                    }
+                //Save to localstore
+                let rang = [];
+                let $card = $('.dashboard').find('.well');
+                $card.each(function (indx, elem) {
+                    rang.push(1 + $card.index(elem) + " ");
                 });
-                localStorage.arrHideWell = JSON.stringify(arrHideWell);
+                localStorage.setItem('rang', JSON.stringify(rang));
+                console.log(rang);
+                let order = sortable.toArray();
+                console.log(order);
+
+                // let $arrWell = $('.dashboard').find('.well'),
+                //     arrHideWell = [];
+                // $arrWell.each(function (index) {
+                //     if ($(this).hasClass('hide')) {
+                //         arrHideWell.push(index);
+                //     }
+                // });
+                // localStorage.arrHideWell = JSON.stringify(arrHideWell);
 
                 $('.dashboard').find('.well.hide').hide();
             });
