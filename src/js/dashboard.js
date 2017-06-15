e928a5ac29b7cf20e9239dd41d4e8fa237da819a
Viacheslav <mukha.viacheslav@gmail.com>
2017-06-15 12:40:38 +0300

    Fixed save to localStorage

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -2,6 +2,7 @@
     $(document).ready(function () {
         "use strict";
         let userId = $('body').attr('data-user');
+        $('.preloader').css('display', 'block');
 
         function getHomeGroupStats() {
             let resData = {
@@ -54,45 +55,46 @@
             });
         }
 
+        function getLocalStorage() {
+            let arrSortClass = localStorage.arrHideWell ? JSON.parse(localStorage.arrHideWell) : [],
+                order = localStorage.order ? JSON.parse(localStorage.order) : [],
+                $arrWell = $('.dashboard').find('.well');
+
+            if (order.length > 0) {
+                for (let i = 0; i < order.length; i++) {
+                    let index = +order[i];
+                    $('#drop').append($arrWell[index]);
+                }
+                for (let i = 0; i < arrSortClass.length; i++) {
+                    let index = arrSortClass[i];
+                    $($arrWell[index]).addClass('hide').hide().find('.vision').addClass('active');
+                }
+            }
+        }
+
         function initSortable() {
             let sortCard = document.getElementById('drop'),
-                arrSortClass = localStorage.arrHideWell ? JSON.parse(localStorage.arrHideWell) : [];
-            let sortable = new Sortable(sortCard, {
-                sort: true,
-                animation: 150,
-                ghostClass: 'sortable-ghost',
-                draggable: '.well',
-                chosenClass: "sortable-chosen",
-                dragClass: "sortable-drag",
-                group: "localStorage-example",
-                // store: {
-                //     get: function (sortable) {
-                //         let order = localStorage.getItem(sortable.options.group.name);
-                //         return order ? order.split('|') : [];
-                //     },
-                //     set: function (sortable) {
-                //         // let order = sortable.toArray();
-                //         // localStorage.setItem(sortable.options.group.name, order.join('|'));
-                //     }
-                // },
-                filter: ".vision",
-	            onFilter: function (evt) {
-		            let item = evt.item,
-			            ctrl = evt.target;
-		            if (Sortable.utils.is(ctrl, ".vision")) {
-                        $(item).find('.vision').toggleClass('active');
-                        $(item).closest('.well').toggleClass('hide');
-		            }
-                }
+                sortable = new Sortable(sortCard, {
+                    sort: true,
+                    animation: 150,
+                    ghostClass: 'sortable-ghost',
+                    draggable: '.well',
+                    chosenClass: "sortable-chosen",
+                    dragClass: "sortable-drag",
+                    group: "localStorage-example",
+                    filter: ".vision",
+                    onFilter: function (evt) {
+                        let item = evt.item,
+                            ctrl = evt.target;
+                        if (Sortable.utils.is(ctrl, ".vision")) {
+                            $(item).find('.vision').toggleClass('active');
+                            $(item).closest('.well').toggleClass('hide');
+                        }
+                    }
             });
 
             sortable.option("disabled", true);
-            let $arrWell = $('.dashboard').find('.well');
 
-            for (let i = 0; i < arrSortClass.length; i++) {
-                let index = arrSortClass[i];
-                $($arrWell[index]).addClass('hide').hide().find('.vision').addClass('active');
-            }
             $('.dashboard').find('.edit-desk').on('click', function () {
                 $(this).toggleClass('active');
                 $('.dashboard').find('.drop').toggleClass('active');
@@ -109,29 +111,18 @@
                 $('.dashboard').find('.edit-desk').removeClass('active');
                 $('.dashboard').find('.drop').removeClass('active');
                 let state = sortable.option("disabled");
-                // let order = sortable.toArray();
-                sortable.option("disabled", !state);
-                // localStorage.setItem(sortable.options.group.name, order.join('|'));
-
-                //Save to localstore
-                let rang = [];
-                let $card = $('.dashboard').find('.well');
-                $card.each(function (indx, elem) {
-                    rang.push(1 + $card.index(elem) + " ");
-                });
-                localStorage.setItem('rang', JSON.stringify(rang));
-                console.log(rang);
                 let order = sortable.toArray();
-                console.log(order);
+                sortable.option("disabled", !state);
+                localStorage.order = JSON.stringify(order);
 
-                // let $arrWell = $('.dashboard').find('.well'),
-                //     arrHideWell = [];
-                // $arrWell.each(function (index) {
-                //     if ($(this).hasClass('hide')) {
-                //         arrHideWell.push(index);
-                //     }
-                // });
-                // localStorage.arrHideWell = JSON.stringify(arrHideWell);
+                let $arrWell = $('.dashboard').find('.well'),
+                    arrHideWell = [];
+                $arrWell.each(function (index) {
+                    if ($(this).hasClass('hide')) {
+                        arrHideWell.push(index);
+                    }
+                });
+                localStorage.arrHideWell = JSON.stringify(arrHideWell);
 
                 $('.dashboard').find('.well.hide').hide();
             });
@@ -145,8 +136,12 @@
             let tmpl = document.getElementById('mainStatisticsTmp').innerHTML;
             let rendered = _.template(tmpl)(data);
             $('#dashboard').append(rendered);
+            getLocalStorage();
             initSortable();
-        });
+            $('.preloader').css('display', 'none');
+        }).catch(function (err) {
+        console.log(err);
+    });
 
         $('#master-filter').select2();
 
