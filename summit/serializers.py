1fd400a5c539b43a5a9af21af15dd2f647cfba00
woobinda <woobinda@voliacable.com>
2017-06-16 17:16:16 +0300

    add e_ticket to summit serializer

--- summit/serializers.py
+++ summit/serializers.py
@@ -52,6 +52,7 @@ class SummitAnketSerializer(serializers.HyperlinkedModelSerializer):
     repentance_date = serializers.CharField(source='user.repentance_date')
     spiritual_level = serializers.CharField(source='get_spiritual_level_display')
     ticket_status = ReadOnlyChoiceWithKeyField(choices=SummitAnket.TICKET_STATUSES, read_only=True)
+    reg_code_requested = serializers.BooleanField(source='status.reg_code_requested', read_only=True)
 
     class Meta:
         model = SummitAnket
@@ -64,7 +65,7 @@ class SummitAnketSerializer(serializers.HyperlinkedModelSerializer):
                   'link',
                   'ticket_status',
 
-                  'total_sum',
+                  'total_sum', 'reg_code_requested',
                   )
 
 
