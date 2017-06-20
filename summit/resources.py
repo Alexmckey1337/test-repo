9dbf8309afdd50c7849624ae0c633a306e220a88
pluton <amirrpp@yandex.ru>
2017-06-20 10:08:00 +0300

    [summit] fix summit export

--- summit/resources.py
+++ summit/resources.py
@@ -1,19 +1,68 @@
 # -*- coding: utf-8
 from __future__ import unicode_literals
 
-from django.utils import six
+from import_export import fields
 
-from account.resources import UserResource, USER_RESOURCE_FIELDS, UserMetaclass
+from common.resources import CustomFieldsModelResource
 from .models import SummitAnket
 
 
-class SummitAnketResource(six.with_metaclass(UserMetaclass, UserResource)):
+class SummitAnketResource(CustomFieldsModelResource):
     """For excel import/export"""
+    full_name = fields.Field()
+    e_ticket = fields.Field()
+    email = fields.Field()
+    phone_number = fields.Field()
+    district = fields.Field()
+    address = fields.Field()
+    region = fields.Field()
+    repentance_date = fields.Field()
+    born_date = fields.Field()
 
     user_field_name = 'user'
 
     class Meta:
         model = SummitAnket
-        fields = USER_RESOURCE_FIELDS + (
-            'code', 'ticket_status'
+        fields = (
+            'full_name', 'email', 'phone_number',
+            'e_ticket',
+            'district', 'address', 'region',
+            'repentance_date', 'born_date',
+
+            'hierarchy_title', 'responsible',
+            'divisions_title', 'spiritual_level', 'city', 'country', 'department',
+            'value', 'description',
+            'code', 'ticket_status',
         )
+
+    def dehydrate_spiritual_level(self, user):
+        return user.get_spiritual_level_display()
+
+    def dehydrate_full_name(self, profile):
+        return profile.user.fullname
+
+    def dehydrate_e_ticket(self, profile):
+        if hasattr(profile, 'status'):
+            return profile.status.reg_code_requested
+        return False
+
+    def dehydrate_district(self, profile):
+        return profile.user.district
+
+    def dehydrate_address(self, profile):
+        return profile.user.address
+
+    def dehydrate_region(self, profile):
+        return profile.user.region
+
+    def dehydrate_repentance_date(self, profile):
+        return profile.user.repentance_date
+
+    def dehydrate_born_date(self, profile):
+        return profile.user.born_date
+
+    def dehydrate_phone_number(self, profile):
+        return profile.user.phone_number
+
+    def dehydrate_email(self, profile):
+        return profile.user.email
