22650f28b059c075d14dc469b5dcbd32fc16b077
woobinda <woobinda@voliacable.com>
2017-06-30 15:47:56 +0300

    Uncomment Celery screepts to create and expired ChurchReports

--- edem/settings/base.py
+++ edem/settings/base.py
@@ -283,16 +283,16 @@ CELERYBEAT_SCHEDULE = {
         'task': 'meetings_to_expired',
         'schedule': crontab(hour=0, minute=0, day_of_week='mon')
     },
-    # # Executes every monday evening at 00:05 A.M
-    # 'create_new_church_reports': {
-    #     'task': 'create_new_meetings',
-    #     'schedule': crontab(hour=0, minute=5, day_of_week='mon')
-    # },
-    # # Executes every monday evening at 00:00 A.M
-    # 'church_reports_to_expired': {
-    #     'task': 'meetings_to_expired',
-    #     'schedule': crontab(hour=0, minute=0, day_of_week='mon')
-    # },
+    # Executes every monday evening at 00:05 A.M
+    'create_new_church_reports': {
+        'task': 'create_new_meetings',
+        'schedule': crontab(hour=0, minute=5, day_of_week='mon')
+    },
+    # Executes every monday evening at 00:00 A.M
+    'church_reports_to_expired': {
+        'task': 'meetings_to_expired',
+        'schedule': crontab(hour=0, minute=0, day_of_week='mon')
+    },
     # Executes every day, every 1 minutes
     # 'get_palace_logs': {
     #     'task': 'get_palace_logs',
