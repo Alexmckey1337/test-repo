3060f4d09b3401d6fb45c738e82f145d0018f520
Viacheslav <mukha.viacheslav@gmail.com>
2017-07-20 11:42:00 +0300

    Fixed yAxes

--- src/js/summit_statistics.js
+++ src/js/summit_statistics.js
@@ -1,6 +1,6 @@
 function initChart(summitId, update = false) {
     getSummitAttends(summitId).then(res => {
-        let labels = _.map(res, (el) => moment(el[0]*1000).format('DD.MM')),
+        let labels = _.map(res, (el) => moment(el[0] * 1000).format('DD.MM')),
             peopleVisit = _.map(res, (el) => el[1][0]),
             peopleAll = _.map(res, (el) => el[1][1]),
             config = setConfig(labels, peopleVisit, peopleAll);
@@ -16,92 +16,100 @@ function updateChart(chart, labels, peopleVisit, peopleAll) {
 }
 
 const CHARTCOLORS = {
-	red: 'rgb(255, 99, 132)',
-	orange: 'rgb(255, 159, 64)',
-	yellow: 'rgb(255, 205, 86)',
-	green: 'rgb(75, 192, 192)',
-	blue: 'rgb(60, 174, 218)',
-	purple: 'rgb(153, 102, 255)',
-	grey: 'rgb(201, 203, 207)'
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(60, 174, 218)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
 };
 
-function setConfig(labels=[], peopleVisit=[], peopleAll=[]) {
+function setConfig(labels = [], peopleVisit = [], peopleAll = []) {
     let config = {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: "Всего людей",
-                    borderColor: CHARTCOLORS.blue,
-                    backgroundColor: CHARTCOLORS.blue,
-                    data: peopleAll,
-                    fill: false,
-                }, {
-                    label: "Присутствовало",
-                    borderColor: CHARTCOLORS.red,
-                    backgroundColor: CHARTCOLORS.red,
-                    data: peopleVisit,
-                    fill: false,
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: "Всего людей",
+                borderColor: CHARTCOLORS.blue,
+                backgroundColor: CHARTCOLORS.blue,
+                data: peopleAll,
+                fill: false,
+            }, {
+                label: "Присутствовало",
+                borderColor: CHARTCOLORS.red,
+                backgroundColor: CHARTCOLORS.red,
+                data: peopleVisit,
+                fill: false,
+            }]
+        },
+        options: {
+            responsive: true,
+            title: {
+                display: true,
+                text: "Статистика посещаемости саммита",
+                fontSize: 18,
+            },
+            legend: {
+                display: true,
+                labels: {
+                    fontSize: 14,
+                }
+            },
+            tooltips: {
+                mode: 'index',
+                callbacks: {
+                    footer: (tooltipItems, data) => {
+                        let all = data.datasets[tooltipItems[0].datasetIndex].data[tooltipItems[0].index],
+                            visit = data.datasets[tooltipItems[1].datasetIndex].data[tooltipItems[1].index],
+                            diff = all - visit;
+                        return `Отсутствовало: ${diff}`;
+                    },
+                },
+                footerFontStyle: 'normal',
+                titleFontSize: 15,
+                bodyFontSize: 13,
+                footerFontSize: 13,
+                titleMarginBottom: 12,
+                bodySpacing: 6,
+            },
+            hover: {
+                mode: 'index',
+                intersect: true
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        show: true,
+                        labelString: 'Day'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        show: true,
+                        labelString: 'Value'
+                    },
+                    ticks: {
+                        min: 0,
+                        callback: function (value) {
+                            if (Math.floor(value) === value) {
+                                return value;
+                            }
+                        }
+                    }
                 }]
             },
-            options: {
-                responsive: true,
-                title: {
-                    display: true,
-                    text: "Статистика посещаемости саммита",
-                    fontSize: 18,
-                },
-                legend: {
-                    display: true,
-                    labels: {
-                        fontSize: 14,
-                    }
-                },
-                tooltips: {
-                    mode: 'index',
-                    callbacks: {
-                        footer: (tooltipItems, data) => {
-                            let all = data.datasets[tooltipItems[0].datasetIndex].data[tooltipItems[0].index],
-                                visit = data.datasets[tooltipItems[1].datasetIndex].data[tooltipItems[1].index],
-                                diff = all - visit;
-                            return `Отсутствовало: ${diff}`;
-                        },
-                    },
-                    footerFontStyle: 'normal',
-                    titleFontSize: 15,
-                    bodyFontSize: 13,
-                    footerFontSize: 13,
-                    titleMarginBottom: 12,
-                    bodySpacing: 6,
-                },
-                hover: {
-                    mode: 'index',
-                    intersect: true
-                },
-                scales: {
-                    xAxes: [{
-                        display: true,
-                        scaleLabel: {
-                            show: true,
-                            labelString: 'Day'
-                        }
-                    }],
-                    yAxes: [{
-                        display: true,
-                        scaleLabel: {
-                            show: true,
-                            labelString: 'Value'
-                        },
-                    }]
-                },
-                elements: {
-                    point: {
-                        radius: 5,
-                        hoverRadius: 7
-                    }
+            elements: {
+                point: {
+                    radius: 5,
+                    hoverRadius: 7
                 }
             }
-        };
+        }
+    };
 
     return config;
 }
@@ -145,7 +153,7 @@ $('document').ready(function () {
 
     $('#print').on('click', function () {
         $('body').addClass('is-print');
-        setTimeout(window.print,1000);
+        setTimeout(window.print, 1000);
     });
 
     (function () {
