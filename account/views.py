3ef458d3c85be363b397c4a39ebbad1c00e4ac40
woobinda <woobinda@voliacable.com>
2017-08-04 17:09:34 +0300

    add pagination

--- account/views.py
+++ account/views.py
@@ -80,6 +80,10 @@ class DuplicatesAvoidedPagination(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
             'count': self.page.paginator.count,
             'results': data
         })
