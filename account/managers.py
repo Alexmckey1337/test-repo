6956551acdb38d1ef140d2cd4d6d3ed330d89caa
pluton <amirrpp@yandex.ru>
2017-08-22 12:59:17 +0300

    migrate CustomUser model from mptt to treebeard.mp_tree

--- account/managers.py
+++ account/managers.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.models import UserManager
-from mptt.managers import TreeManager
-from mptt.querysets import TreeQuerySet
 from rest_framework.compat import is_authenticated
+from treebeard.mp_tree import MP_NodeQuerySet
 
 
-class CustomUserQuerySet(TreeQuerySet):
+class CustomUserQuerySet(MP_NodeQuerySet):
     def base_queryset(self):
         return self
 
@@ -30,7 +29,7 @@ class CustomUserQuerySet(TreeQuerySet):
         return self
 
 
-class CustomUserManager(TreeManager, UserManager):
+class CustomUserManager(UserManager):
     use_in_migrations = False
 
     def get_queryset(self):
