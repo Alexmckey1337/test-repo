fd948cd0884b1ae25056ab30a2aed56f3d4e0308
woobinda <woobinda@voliacable.com>
2017-09-04 11:20:00 +0300

    fix event dashboard_counts

--- event/mixins.py
+++ event/mixins.py
@@ -2,12 +2,21 @@ from rest_framework.generics import get_object_or_404
 from account.models import CustomUser
 
 
-class EventUserTreeSummaryMixin(object):
+class EventUserTreeMixin(object):
     @staticmethod
-    def user_for_tree(request):
+    def master_for_summary(request):
         master_id = request.query_params.get('master_id')
         if master_id:
             user = get_object_or_404(CustomUser, pk=master_id)
+        else:
+            user = request.user
+        return user
+
+    @staticmethod
+    def user_for_dashboard(request):
+        user_id = request.query_params.get('user_id')
+        if user_id:
+            user = get_object_or_404(CustomUser, pk=user_id)
         else:
             user = request.user
         return user
