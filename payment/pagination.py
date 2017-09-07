6551ee18607fc4401fe5d561b53ad4e50800f36a
woobinda <woobinda@voliacable.com>
2017-09-07 19:18:35 +0300

    Add summa (for 30 objects per page) to Payments paginations

--- payment/pagination.py
+++ payment/pagination.py
@@ -7,8 +7,19 @@ from navigation.table_fields import payment_table
 class PaymentPagination(PageNumberPagination):
     page_size = 30
     page_size_query_param = 'page_size'
+    value = 0
 
     def get_paginated_response(self, data):
+        uah = sum([int(x['sum']) for x in data if x['currency_sum']['code'] == 'uah'])
+        rub = sum([int(x['sum']) for x in data if x['currency_sum']['code'] == 'rur'])
+        eur = sum([int(x['sum']) for x in data if x['currency_sum']['code'] == 'eur'])
+        usd = sum([int(x['sum']) for x in data if x['currency_sum']['code'] == 'usd'])
+
+        summa = {'uah': uah,
+                 'rub': rub,
+                 'eur': eur,
+                 'usd': usd}
+
         return Response({
             'links': {
                 'next': self.get_next_link(),
@@ -17,4 +28,5 @@ class PaymentPagination(PageNumberPagination):
             'count': self.page.paginator.count,
             'table_columns': payment_table(self.request.user),
             'results': data,
+            'summa': summa,
         })
