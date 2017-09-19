ca134fe0890b5ef18f50e6966d8933fe6bfb51aa
Viacheslav <mukha.viacheslav@gmail.com>
2017-09-19 16:34:01 +0300

    Fix

---
+++ src/js/modules/sendPassToEmail.js
@@ -0,0 +1,23 @@
+export default function sendPassToEmail() {
+
+    let data = {
+        'email': document.getElementById('send_letter').value
+    };
+
+    let json = JSON.stringify(data);
+    ajaxRequest(URLS.password_forgot(), json, function (data) {
+        showPopup(data.detail);
+    }, 'POST', true, {
+        'Content-Type': 'application/json'
+    }, {
+        400: function (data) {
+            data = data.responseJSON;
+            showPopup(data.detail);
+
+        },
+        500: function () {
+            $('.account .invalid').html('Проверьте введённый e-mail').show();
+            $('#send_letter').val('');
+        }
+    });
+}
--- src/js/restorepass.js
+++ src/js/restorepass.js
@@ -1,26 +1,4 @@
-function sendPassToEmail() {
-
-    let data = {
-        'email': document.getElementById('send_letter').value
-    };
-
-    let json = JSON.stringify(data);
-    ajaxRequest(URLS.password_forgot(), json, function (data) {
-        showPopup(data.detail);
-    }, 'POST', true, {
-        'Content-Type': 'application/json'
-    }, {
-        400: function (data) {
-            data = data.responseJSON;
-            showPopup(data.detail);
-
-        },
-        500: function () {
-            $('.account .invalid').html('Проверьте введённый e-mail').show();
-            $('#send_letter').val('');
-        }
-    });
-}
+import sendPassToEmail from './modules/sendPassToEmail';
 
 $("document").ready(function () {
     document.getElementById('getpass').addEventListener('click', function () {
