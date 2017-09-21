d7396887cba4ddf5f1c3ef10924e09bc89daa515
woobinda <woobinda@voliacable.com>
2017-09-21 12:59:41 +0300

    comment property get_church

--- account/models.py
+++ account/models.py
@@ -129,10 +129,9 @@ class CustomUser(MP_Node, LogModel, User, CustomUserAbstract,
     def link(self):
         return self.get_absolute_url()
 
-    @property
-    def get_church(self):
-        church = self.cchurch or self.hhome_group.church
-        return church
+    # @property
+    # def get_church(self):
+    #     return self.cchurch or self.hhome_group.church
 
     @property
     def column_table(self):
