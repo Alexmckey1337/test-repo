a6ae08a00ea4c1306799b04cc70c77ac2cf10a1c
Viacheslav <mukha.viacheslav@gmail.com>
2017-11-14 17:12:08 +0200

    Fix err in scanner ticket

--- src/js/scanner.js
+++ src/js/scanner.js
@@ -1,7 +1,6 @@
 'use strict';
 import URLS from './modules/Urls/index';
 import getData from './modules/Ajax';
-import {showAlert} from './modules/ShowNotifications/index';
 
 $(document).ready(function () {
     $('#scanner_form').on('submit', function (e) {
@@ -13,22 +12,26 @@ $(document).ready(function () {
                     'Content-Type': 'application/json',
                     'Visitors-Location-Token': '4ewfeciss6qdbmgfj9eg6jb3fdcxefrs4dxtcdrt10rduds2sn'
                 })
-            };
+            },
+            img = $('#user_photo'),
+            imgWrapp = $('.photo_wrapper'),
+            imgUrl =  img.attr('data-img_default');
+
         $('.preloader').css('display', 'block');
         getData(URLS.scan_code(), options, config).then( data => {
-            let img = $('#user_photo'),
-                imgWrapp = $('.photo_wrapper'),
-                imgUrl =  img.attr('data-img_default');
             $('.preloader').css('display', 'none');
             $('#user_name').text(data.fullname);
             $('#count').text(data.passes_count);
             (data.active) ? imgWrapp.removeClass('active') : imgWrapp.addClass('active');
             (data.avatar_url) ? img.attr('src', data.avatar_url) : img.attr('src', imgUrl);
             $('#code').val('').focus();
-        }).catch(err => {
+        }).catch(() => {
             $('.preloader').css('display', 'none');
+            $('#user_name').text('Пользователь не найден');
+            $('#count').text('0');
+            imgWrapp.removeClass('active');
+            img.attr('src', imgUrl);
             $('#code').val('').focus();
-            showAlert(err.detail)
         });
     })
 });
