8f8f3cf514a340e24e858fbc6df27d41b1d50da7
woobinda <woobinda@voliacable.com>
2017-11-27 12:42:07 +0200

    fix styles

--- account/pagination.py
+++ account/pagination.py
@@ -14,3 +14,9 @@ class DashboardPagination(PageNumberPagination):
     page_size = 50
     page_size_query_param = 'page_size'
     max_page_size = 50
+
+
+class UserCallsPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 3
