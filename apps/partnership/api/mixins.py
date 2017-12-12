333cc2ab0b0c1e910597769b3b68d3e6be362c73
pluton <amirrpp@yandex.ru>
2017-12-12 10:36:12 +0200

    added stats by church partners to /api/managers_summary/

--- apps/partnership/api/mixins.py
+++ apps/partnership/api/mixins.py
@@ -19,15 +19,15 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 
 from apps.account.models import CustomUser
-from common.views_mixins import ExportViewSetMixin
-from apps.partnership.models import Partnership, Deal, PartnershipLogs
-from apps.payment.api.views_mixins import CreatePaymentMixin, ListPaymentMixin
+from apps.navigation.table_columns import get_table
 from apps.partnership.api.permissions import (
     CanSeePartnerStatistics, CanCreatePartnerPayment, CanSeeDealPayments,
     CanExportPartnerList, CanSeeManagerSummary, CanCreateChurchPartnerPayment, CanSeeChurchDealPayments,
     CanExportChurchPartnerList)
+from apps.partnership.models import Partnership, Deal, PartnershipLogs, ChurchDeal, ChurchPartner, ChurchPartnerLog
+from apps.payment.api.views_mixins import CreatePaymentMixin, ListPaymentMixin
 from apps.payment.models import Payment, Currency
-from apps.navigation.table_columns import get_table
+from common.views_mixins import ExportViewSetMixin
 
 sql_logger = logging.Logger('partner.sql')
 
@@ -201,36 +201,48 @@ class ManagerSummaryMixin:
         year = int(request.query_params.get('year', datetime.now().year))
         month = int(request.query_params.get('month', datetime.now().month))
 
-        managers_query, partners, plan = self._get_start_data()
+        managers_query, plan = self._get_managers()
+        partners, church_partners = self._get_partners()
 
-        partners_data = self._get_partners(managers_query)
+        partners_data = self._get_partners_names(managers_query)
         managers = [{
             # Making queries from Partnership model
             'sum_pay': x[0] or 0,
             'sum_pay_tithe': x[1] or 0,
-            'sum_deals': x[2] or 0,
-            'manager': x[3],
-            'user_id': x[4],
+            'sum_pay_church': x[2] or 0,
+            'sum_deals': x[3] or 0,
+            'sum_church_deals': x[4] or 0,
+            'manager': x[5],
+            'user_id': x[6],
             # Making queries from PartnershipLoUntitled Diagram.xmlgs model if requested period not in this month
-            'total_partners': x[5] or 0,
-            'active_partners': x[6] or 0,
-            'potential_sum': x[7] or 0,
-            'plan': x[8] or 0,
+            'total_partners': x[7] or 0,
+            'active_partners': x[8] or 0,
+            'potential_sum': x[9] or 0,
+            'total_church_partners': x[10] or 0,
+            'active_church_partners': x[11] or 0,
+            'church_potential_sum': x[12] or 0,
+            'plan': x[13] or 0,
 
         } for x in zip(
             self._get_sum_pay(year, month, deal_type=Deal.DONATION),
             self._get_sum_pay(year, month, deal_type=Deal.TITHE),
+            self._get_sum_church_pay(year, month),
             self._get_sum_deals(managers_query, year, month),
+            self._get_sum_church_deals(managers_query, year, month),
             [p['full_name'] for p in partners_data],
             [p['id'] for p in partners_data],
             self._get_total_partners(partners, managers_query),
             self._get_active_partners(partners, managers_query),
             self._get_potential_sum(partners, managers_query),
+            self._get_total_church_partners(church_partners, managers_query),
+            self._get_active_church_partners(church_partners, managers_query),
+            self._get_church_potential_sum(church_partners, managers_query),
             plan,
         )]
         managers = [manager for manager in managers if (
                 manager['potential_sum'] != 0 or
                 manager['sum_pay'] != 0 or
+                manager['sum_pay_church'] != 0 or
                 manager['sum_pay_tithe'] != 0)]
         managers = self._order_managers(managers)
 
@@ -404,23 +416,34 @@ class ManagerSummaryMixin:
             result = connect.fetchall()
         return {date: sum for date, sum in result}
 
-    def _get_start_data(self):
+    def _get_managers(self):
         year = int(self.request.query_params.get('year', datetime.now().year))
         month = int(self.request.query_params.get('month', datetime.now().month))
 
         manager_ids = CustomUser.objects.filter(Q(partner_role__isnull=False) | Q(
-            disciples_deals__isnull=False)).distinct().values_list('id', flat=True)
+            disciples_deals__isnull=False) | Q(
+            partner_disciples_deals__isnull=False)).distinct().values_list('id', flat=True)
 
         managers = CustomUser.objects.filter(id__in=manager_ids).order_by('pk')
 
         plan = self._get_managers_plan(managers)
+
+        if year != datetime.now().year or month != datetime.now().month:
+            next_month = self.get_next_month(year, month)
+            managers, plan = self.get_logged_managers(next_month)
+        return managers, plan
+
+    def _get_partners(self):
+        year = int(self.request.query_params.get('year', datetime.now().year))
+        month = int(self.request.query_params.get('month', datetime.now().month))
         partners = Partnership.objects.all()
+        church_partners = ChurchPartner.objects.all()
 
         if year != datetime.now().year or month != datetime.now().month:
             next_month = self.get_next_month(year, month)
             partners = self.get_logged_partners(next_month)
-            managers, plan = self.get_logged_managers(next_month)
-        return managers, partners, plan
+            church_partners = self.get_logged_church_partners(next_month)
+        return partners, church_partners
 
     def _order_managers(self, managers):
         ordering = self.request.query_params.get('ordering', 'manager')
@@ -432,7 +455,7 @@ class ManagerSummaryMixin:
         return managers
 
     @staticmethod
-    def _get_partners(queryset):
+    def _get_partners_names(queryset):
         return queryset.annotate(full_name=Concat(
             'last_name', V(' '), 'first_name', V(' '), 'middle_name')).values(
             'full_name', 'id').order_by('pk')
@@ -452,6 +475,7 @@ class ManagerSummaryMixin:
           WHERE u.user_ptr_id in (
               SELECT uu.user_ptr_id from account_customuser uu
               LEFT OUTER JOIN partnership_deal d ON (uu.user_ptr_id = d.responsible_id)
+              LEFT OUTER JOIN partnership_churchdeal cd ON (uu.user_ptr_id = cd.responsible_id)
               LEFT OUTER JOIN partnership_partnerrolelog pr ON (uu.user_ptr_id = pr.user_id and pr.id IN (
                 SELECT DISTINCT ON (user_id) (
                   SELECT pl.id
@@ -459,9 +483,39 @@ class ManagerSummaryMixin:
                   WHERE p.user_id = pl.user_id and pl.log_date < '{next_month}-01'
                   ORDER BY pl.log_date DESC LIMIT 1
                 ) FROM partnership_partnerrolelog p) and pr.deleted = false)
-              WHERE pr.id IS NOT NULL OR d.id IS NOT NULL)
+              WHERE pr.id IS NOT NULL OR d.id IS NOT NULL OR cd.id IS NOT NULL)
           ORDER BY u.user_ptr_id;
           """.format(type=deal_type, year=year, month=month, next_month=self.get_next_month(year, month))
+
+        with connection.cursor() as connect:
+            connect.execute(raw)
+            result = connect.fetchall()
+
+        for k in result:
+            yield k[1]
+
+    def _get_sum_church_pay(self, year, month):
+        raw = """
+          select u.user_ptr_id,
+          (select sum(pay.sum) from payment_payment pay WHERE pay.content_type_id = 100 and
+          pay.object_id in (select d.id from partnership_churchdeal d where d.responsible_id = u.user_ptr_id and
+          (d.date_created BETWEEN '{year}-01-01' and '{year}-12-31') and
+          extract('month' from d.date_created) = {month} )) sum
+          from account_customuser u
+          WHERE u.user_ptr_id in (
+              SELECT uu.user_ptr_id from account_customuser uu
+              LEFT OUTER JOIN partnership_deal d ON (uu.user_ptr_id = d.responsible_id)
+              LEFT OUTER JOIN partnership_churchdeal cd ON (uu.user_ptr_id = cd.responsible_id)
+              LEFT OUTER JOIN partnership_partnerrolelog pr ON (uu.user_ptr_id = pr.user_id and pr.id IN (
+                SELECT DISTINCT ON (user_id) (
+                  SELECT pl.id
+                  FROM partnership_partnerrolelog pl
+                  WHERE p.user_id = pl.user_id and pl.log_date < '{next_month}-01'
+                  ORDER BY pl.log_date DESC LIMIT 1
+                ) FROM partnership_partnerrolelog p) and pr.deleted = false)
+              WHERE pr.id IS NOT NULL OR d.id IS NOT NULL OR cd.id IS NOT NULL)
+          ORDER BY u.user_ptr_id;
+          """.format(year=year, month=month, next_month=self.get_next_month(year, month))
 
         with connection.cursor() as connect:
             connect.execute(raw)
@@ -479,9 +533,35 @@ class ManagerSummaryMixin:
             sum_deals=Subquery(subqs_deals, output_field=DecimalField())).values_list(
             'sum_deals', flat=True)
 
+    @staticmethod
+    def _get_sum_church_deals(queryset, year, month):
+        subqs_deals = ChurchDeal.objects.filter(
+            date_created__year=year, date_created__month=month, responsible=OuterRef('pk')
+        ).order_by().values('responsible').annotate(deals_sum=Sum('value')).values('deals_sum')
+
+        return queryset.annotate(
+            sum_deals=Subquery(subqs_deals, output_field=DecimalField())).values_list(
+            'sum_deals', flat=True)
+
     """
     Making queries from PartnershipLogs model if requested period not in this month
     """
+
+    # @staticmethod
+    # def _get_sum_deals(queryset, year, month):
+    #     subqs_deals = Deal.objects.filter(date_created__year=year, date_created__month=month, responsible=OuterRef(
+    #         'pk')).order_by().values('responsible').annotate(deals_sum=Sum('value')).values('deals_sum')
+    #     subqs_church_deals = ChurchDeal.objects.filter(
+    #         date_created__year=year, date_created__month=month, responsible=OuterRef('pk')
+    #     ).order_by().values('responsible').annotate(church_deals_sum=Sum('value')).values('church_deals_sum')
+    #
+    #     z = zip(
+    #         queryset.annotate(
+    #             sum_deals=Subquery(subqs_deals, output_field=DecimalField())).values_list('sum_deals', flat=True),
+    #         queryset.annotate(
+    #             sums=Subquery(subqs_church_deals, output_field=DecimalField())).values_list('sums', flat=True),
+    #     )
+    #     return map(lambda d: sum([d[0] or 0, d[1] or 0]), z)
 
     @staticmethod
     @func_time
@@ -506,6 +586,27 @@ class ManagerSummaryMixin:
 
     @staticmethod
     @func_time
+    def get_logged_church_partners(next_month):
+        raw = """
+              SELECT (SELECT pl.id FROM partnership_churchpartnerlog pl WHERE p.id = pl.partner_id AND
+              pl.log_date < '%s-01'
+              ORDER BY log_date DESC LIMIT 1
+              ) p_log_id
+              FROM partnership_churchpartner p ORDER BY p.id;
+              """ % next_month
+
+        with connection.cursor() as connect:
+            connect.execute(raw)
+            result = connect.fetchall()
+
+        logged_ids = [k[0] for k in result]  # from arrays with tuples of ids to array with integer ids
+
+        logged_queryset = ChurchPartnerLog.objects.filter(id__in=logged_ids)
+
+        return logged_queryset
+
+    @staticmethod
+    @func_time
     def get_logged_managers(next_month):
         raw = """
                 SELECT log.user_id, log.plan FROM partnership_partnerrolelog log WHERE id IN (
@@ -523,7 +624,8 @@ class ManagerSummaryMixin:
 
         managers_query = list(set(CustomUser.objects.filter(
             Q(id__in=[k[0] for k in result]) |
-            Q(disciples_deals__isnull=False)
+            Q(disciples_deals__isnull=False) |
+            Q(partner_disciples_deals__isnull=False)
         ).order_by().only('pk').distinct('pk').values_list('id', flat=True)))
         plan = []
         for m in sorted(managers_query):
@@ -545,6 +647,24 @@ class ManagerSummaryMixin:
         if month == 12:
             return '{year}-{month}'.format(year=year + 1, month=1)
         return '{year}-{month}'.format(year=year, month=month + 1)
+
+    @staticmethod
+    def _get_total_church_partners(partners, managers):
+        s = dict(Counter(list(partners.order_by().values_list('responsible', flat=True))))
+        for m in managers:
+            yield s.get(m.pk, 0)
+
+    @staticmethod
+    def _get_active_church_partners(partners, managers):
+        s = dict(Counter(list(partners.filter(is_active=True).order_by().values_list('responsible', flat=True))))
+        for m in managers:
+            yield s.get(m.pk, 0)
+
+    @staticmethod
+    def _get_church_potential_sum(partners, managers):
+        s = list(partners.order_by().values('responsible', 'value'))
+        for m in managers:
+            yield sum([f['value'] for f in filter(lambda z: z['responsible'] == m.pk, s)])
 
     @staticmethod
     def _get_total_partners(partners, managers):
