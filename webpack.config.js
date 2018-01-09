484b1cd94944fdeeaa0401b68022c8a07303fe1b
kolintsio96 <kolintsiovolodymyr@gmail.com>
2018-01-09 17:09:15 +0200

    add template

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     entry: {
         entry: ['babel-polyfill', './entry.js'],
         tasks: ['babel-polyfill', './tasks.js'],
+        db_access: ['babel-polyfill', './db_access.js'],
         restorepass: ['babel-polyfill', './restorepass.js'],
         editpass: ['babel-polyfill', './editpass.js'],
         main: ['babel-polyfill', './main.js'],
