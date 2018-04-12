b91c1323124f4feabe865fe36130c8f13796953e
pluton <amirrpp@yandex.ru>
2018-04-12 12:25:22 +0300

    added Hierarchy.code field

--- apps/hierarchy/models.py
+++ apps/hierarchy/models.py
@@ -11,6 +11,7 @@ class Department(models.Model):
 class Hierarchy(models.Model):
     title = models.CharField(max_length=50, unique=True)
     level = models.IntegerField()
+    code = models.SlugField('Code', max_length=50, blank=True)
 
     class Meta:
         ordering = ['level']
