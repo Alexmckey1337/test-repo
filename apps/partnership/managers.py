5bef80e6ed7fb8be970abab12583203af5cfd354
pluton <amirrpp@yandex.ru>
2018-04-26 09:53:59 +0300

    refactoring

--- apps/partnership/managers.py
+++ apps/partnership/managers.py
@@ -9,23 +9,23 @@ class DealQuerySet(models.query.QuerySet):
             'partnership', 'responsible',
             'partnership__user', 'currency')
 
-    def annotate_full_name(self):
+    def annotate_full_name(self, alias='full_name'):
         return self.annotate(
-            full_name=Concat(
+            **{alias: Concat(
                 'partnership__user__last_name', V(' '),
                 'partnership__user__first_name', V(' '),
-                'partnership__user__middle_name'))
+                'partnership__user__middle_name')})
 
-    def annotate_responsible_name(self):
+    def annotate_responsible_name(self, alias='responsible_name'):
         return self.annotate(
-            responsible_name=Concat(
+            **{alias: Concat(
                 'responsible__last_name', V(' '),
                 'responsible__first_name', V(' '),
-                'responsible__middle_name')
+                'responsible__middle_name')}
         )
 
-    def annotate_total_sum(self):
-        return self.annotate(total_sum=Coalesce(Sum('payments__effective_sum'), V(0)))
+    def annotate_total_sum(self, alias='total_sum'):
+        return self.annotate(**{alias: Coalesce(Sum('payments__effective_sum'), V(0))})
 
     def for_user(self, user, extra_perms=True):
         if not user.is_authenticated or not user.has_partner_role:
@@ -42,14 +42,14 @@ class DealManager(models.Manager):
     def base_queryset(self):
         return self.get_queryset().base_queryset()
 
-    def annotate_full_name(self):
-        return self.get_queryset().annotate_full_name()
+    def annotate_full_name(self, alias='full_name'):
+        return self.get_queryset().annotate_full_name(alias=alias)
 
-    def annotate_responsible_name(self):
-        return self.get_queryset().annotate_responsible_name()
+    def annotate_responsible_name(self, alias='responsible_name'):
+        return self.get_queryset().annotate_responsible_name(alias=alias)
 
-    def annotate_total_sum(self):
-        return self.get_queryset().annotate_total_sum()
+    def annotate_total_sum(self, alias='total_sum'):
+        return self.get_queryset().annotate_total_sum(alias=alias)
 
     def for_user(self, user, extra_perms=True):
         return self.get_queryset().for_user(user=user, extra_perms=extra_perms)
@@ -62,12 +62,12 @@ class PartnerQuerySet(models.query.QuerySet):
             'currency') \
             .prefetch_related('user__divisions', 'user__departments')
 
-    def annotate_full_name(self):
+    def annotate_full_name(self, alias='full_name'):
         return self.annotate(
-            full_name=Concat(
+            **{alias: Concat(
                 'user__last_name', V(' '),
                 'user__first_name', V(' '),
-                'user__middle_name'))
+                'user__middle_name')})
 
     def for_user(self, user, extra_perms=True):
         if not user.is_authenticated or not user.has_partner_role:
@@ -84,8 +84,8 @@ class PartnerManager(models.Manager):
     def base_queryset(self):
         return self.get_queryset().base_queryset()
 
-    def annotate_full_name(self):
-        return self.get_queryset().annotate_full_name()
+    def annotate_full_name(self, alias='full_name'):
+        return self.get_queryset().annotate_full_name(alias=alias)
 
     def for_user(self, user, extra_perms=True):
         return self.get_queryset().for_user(user=user, extra_perms=extra_perms)
@@ -97,20 +97,20 @@ class ChurchDealQuerySet(models.query.QuerySet):
             'partnership', 'responsible',
             'partnership__church', 'currency')
 
-    def annotate_full_name(self):
+    def annotate_full_name(self, alias='full_name'):
         return self.annotate(
-            full_name=F('partnership__church__title'))
+            **{alias: F('partnership__church__title')})
 
-    def annotate_responsible_name(self):
+    def annotate_responsible_name(self, alias='responsible_name'):
         return self.annotate(
-            responsible_name=Concat(
+            **{alias: Concat(
                 'responsible__last_name', V(' '),
                 'responsible__first_name', V(' '),
-                'responsible__middle_name')
+                'responsible__middle_name')}
         )
 
-    def annotate_total_sum(self):
-        return self.annotate(total_sum=Coalesce(Sum('payments__effective_sum'), V(0)))
+    def annotate_total_sum(self, alias='total_sum'):
+        return self.annotate(**{alias: Coalesce(Sum('payments__effective_sum'), V(0))})
 
     def for_user(self, user, extra_perms=True):
         if not user.is_authenticated or not user.is_partner:
@@ -127,14 +127,14 @@ class ChurchDealManager(models.Manager):
     def base_queryset(self):
         return self.get_queryset().base_queryset()
 
-    def annotate_full_name(self):
-        return self.get_queryset().annotate_full_name()
+    def annotate_full_name(self, alias='full_name'):
+        return self.get_queryset().annotate_full_name(alias=alias)
 
-    def annotate_responsible_name(self):
-        return self.get_queryset().annotate_responsible_name()
+    def annotate_responsible_name(self, alias='responsible_name'):
+        return self.get_queryset().annotate_responsible_name(alias=alias)
 
-    def annotate_total_sum(self):
-        return self.get_queryset().annotate_total_sum()
+    def annotate_total_sum(self, alias='total_sum'):
+        return self.get_queryset().annotate_total_sum(alias=alias)
 
     def for_user(self, user, extra_perms=True):
         return self.get_queryset().for_user(user=user, extra_perms=extra_perms)
