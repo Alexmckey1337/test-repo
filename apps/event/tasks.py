f8f650a3806bfa8a0cf2a38b5bf0eb68d2df5108
kovtyuh <kovtyuh.marina@gmail.com>
2018-09-05 17:01:57 +0300

    Create task for home reports type "night"

--- apps/event/tasks.py
+++ apps/event/tasks.py
@@ -56,6 +56,30 @@ def processing_home_service_meetings():
         print(e)
 
 
+@app.task(name='processing_home_service_meetings', ignore_result=True,
+          max_retries=10, default_retry_delay=1000)
+def processing_home_night_meetings():
+    """
+    Report for home meeting type "night"
+    """
+    current_date = timezone.now().date()
+    active_home_groups = HomeGroup.objects.filter(active=True)
+    meeting_type = MeetingType.objects.get(code='night')
+
+    try:
+        with transaction.atomic():
+            expired_reports = Meeting.objects.filter(status=Meeting.IN_PROGRESS)
+            expired_reports.update(status=Meeting.EXPIRED)
+
+            for home_group in active_home_groups:
+                Meeting.objects.get_or_create(home_group=home_group,
+                                              owner=home_group.leader,
+                                              date=current_date,
+                                              type=meeting_type)
+    except IntegrityError as e:
+        print(e)
+
+
 # CHURCH REPORT TASKS
 
 @app.task(name='processing_church_reports', ignore_result=True,
